348405d377095a6a5eff40ff9f17c65815d0bafc
Diego Chagas <drochachagas@gmail.com>
2020-02-24 05:19:03 -0300

    Updates

---
+++ precache-manifest.02e022900b00cc998905e7ec7c5d067c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c4680025893250ed67f7666df71a636",
+    "url": "/character-sheet/index.html"
+  },
+  {
+    "revision": "73d39116c76350dcf462",
+    "url": "/character-sheet/static/css/2.c6250e3a.chunk.css"
+  },
+  {
+    "revision": "73d39116c76350dcf462",
+    "url": "/character-sheet/static/js/2.d16493b4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/character-sheet/static/js/2.d16493b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "376cfaba3c3d76dfc0d5",
+    "url": "/character-sheet/static/js/main.16a55aad.chunk.js"
+  },
+  {
+    "revision": "8643dfd83eb708cafbf0",
+    "url": "/character-sheet/static/js/runtime-main.19350f09.js"
+  },
+  {
+    "revision": "15bdff4ef6ae448e519f24238b4c9311",
+    "url": "/character-sheet/static/media/microgrammad-bold-extented.15bdff4e.ttf"
+  },
+  {
+    "revision": "49441315ab9ce55176682715f25ea4b2",
+    "url": "/character-sheet/static/media/microgrammad-bold-extented.49441315.svg"
+  },
+  {
+    "revision": "69e60811e8b67318c43ca3228a132144",
+    "url": "/character-sheet/static/media/microgrammad-bold-extented.69e60811.woff"
+  },
+  {
+    "revision": "6a395ffe174d34643f4c880d58f1b4fd",
+    "url": "/character-sheet/static/media/microgrammad-bold-extented.6a395ffe.eot"
+  },
+  {
+    "revision": "5f5609fbfda8bd33f3b3bb5d9b768eaf",
+    "url": "/character-sheet/static/media/milford-condensed-bold.5f5609fb.eot"
+  },
+  {
+    "revision": "866992a7f934b2ae2b82a41c4d5d088f",
+    "url": "/character-sheet/static/media/milford-condensed-bold.866992a7.woff"
+  },
+  {
+    "revision": "a9caf097bc61105b9073dc611b878fbd",
+    "url": "/character-sheet/static/media/milford-condensed-bold.a9caf097.ttf"
+  },
+  {
+    "revision": "e67abedd4616fc61ee73685a5ca3e354",
+    "url": "/character-sheet/static/media/milford-condensed-bold.e67abedd.svg"
+  },
+  {
+    "revision": "5b54ce4f8c62da48a49d6ff1fd53eb42",
+    "url": "/character-sheet/static/media/milford-condensed.5b54ce4f.woff"
+  },
+  {
+    "revision": "65c3f3703a699e5262d179e6ef847858",
+    "url": "/character-sheet/static/media/milford-condensed.65c3f370.ttf"
+  },
+  {
+    "revision": "e43ab4aad7c17d8f8cae1f9091ca8bbc",
+    "url": "/character-sheet/static/media/milford-condensed.e43ab4aa.eot"
+  },
+  {
+    "revision": "e8680053a4a93662ef52831d5b77c4e3",
+    "url": "/character-sheet/static/media/milford-condensed.e8680053.svg"
+  }
+]);
